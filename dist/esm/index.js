009c50f7c4747e6127a863f2246c772f125dd5f7
Nate D <nate@idontchop.com>
2022-04-14 14:56:38 -0700

    Initial Commit

--- dist/esm/index.js
+++
@@ -1,2 +0,0 @@
-import e from"react";const l=l=>e.createElement("div",null,e.createElement("p",null,l.title),e.createElement("p",null,"Hello World, react-scroll-presentation"));export{l as HelloWorld};
-//# sourceMappingURL=index.js.map
