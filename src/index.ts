9b8a6fa021abc0e091071bb34e6e5c965ce2edb3
Nate D <nate@idontchop.com>
2022-04-14 16:58:12 -0700

    rollup babel

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export {default as HelloWorld} from './HelloWorld'
 export {default as Presentation} from './components/Presentation.jsx'
+export {default as ConfigurableSlide} from './ConfigurableSlide'
